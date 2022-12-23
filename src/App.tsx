d34a382d8b15af3a95cb65f360846b91030cbfa1
denukedissanayake <ddnadhananja@gmail.com>
2022-12-24 00:52:29 +0530

    Added the Signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Login from "./pages/login/login";
 import "./App.scss";
+import Login from "./pages/login/login";
+import Signup from "./pages/signup/signup";
 
 const App = () => {
   return (
     <div className="App">
-      <Login/>
+      <Signup/>
     </div>
   );
 }
--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -2,7 +2,26 @@ import "./signup.scss";
 
 const Signup = () => {
   return (
-    <div className="signup">signup</div>
+    <div className="signup">
+      <div className="card">
+        <div className="left">
+          <h1>Register Now</h1>
+          <form>
+            <input type="text" placeholder="Name"/>
+            <input type="text" placeholder="Username"/>
+            <input type="email" placeholder="Email"/>
+            <input type="password" placeholder="Password"/>
+            <button>Signup</button>
+          </form>
+        </div>
+        <div className="right">
+          <h1>Social App</h1>
+          <p>Connect World Together with Social App</p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
